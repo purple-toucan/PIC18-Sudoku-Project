a42bcfbf6dc705d45b21d0f02f51c9164600b280
Jac <Jac@Bethania>
2020-01-14 13:17:12 +0000

    Delay Routine Added

--- Counter.X/Counter_Program.asm
+++ Counter.X/Counter_Program.asm
@@ -6,6 +6,17 @@
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
+bdelay
+	movlw	high(0xDEAD)	    ; Load 16 bit number across 2 FRs
+	movwf	0x10, ACCESS	    
+	movlw	low(0xDEAD)
+	movwf	0x11, ACCESS		    
+	movlw	0x00		    ; Set W = 0
+bdlp	decf	0x11,f		    ; Decrease least significant byte by 1
+	subwfb	0x10,f		    ; Subtract W and borrow bit
+	bc	bdlp		    ; If carry then branch - Borrow = NOT Carry
+	return			    ; Exit subroutine
+	
 start	;HARDWARE SETUP
 	movlw 	0xFF
 	movwf	TRISD, ACCESS	    ; Port D all inputs
@@ -17,6 +28,7 @@ start	;HARDWARE SETUP
 	
 loop	;COUNTING LOOP
 	movff 	0x06, PORTC	    ; Set Port C value
+	call	bdelay
 	incf 	0x06, F, ACCESS	    ; Incriment counter and place back in F
 	movf	PORTD, W	    ; Read Port D to W
 	movwf   PORTE, ACCESS	    ; Port E mirrors D
