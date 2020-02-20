ac4e14be4b1529749c8cbbdd21cc457a9e1a377a
Jac <Jac@Bethania>
2020-02-20 12:05:23 +0000

    20th Feb - write message module

--- Sudoku-v1.X/Write_Message.asm
+++ Sudoku-v1.X/Write_Message.asm
@@ -3,73 +3,33 @@
 	extern Set_GLCD_Cursor_X, Set_GLCD_Cursor_Y
 	extern Write_Block_2
 	
-
-;	
-;	
-;Storage	code    ; a section of programme memory for storing data
-;	; ******* myTable, data in programme memory, and its length *****
-;Y_Table_PM  data .3,.9,.15,.23,.29,.35,.43,.49,.55
-;	    ; y adress (on GLCD) of left side pixel of 5x5 sudoku cell
-;	    ; for sudoku columns 0-8
-;	    constant    Y_Table_length = 9
-;	    
-;;X_Table_PM  data 0,1,1,2,3,4,5,6,6, 5,5,1,1,3,5,5,5,1, 3,1,7,7,5,3,3,1,7
-;X_Table_PM  data .0,.1,.1,.2,.3,.4,.5,.6,.6, .5,.5,.1,.1,.3,.5,.5,.5,.1, .3,.1,.7,.7,.5,.3,.3,.1,.7
-;	    ; x adress (row on GLCD) of top part of 5x5 sudoku cell
-;	    ; how many pixel rows are there of 5x5 shape in this upper row
-;	    ; upoccupied pixel rows above 5x5 cell in upper row
-;	    ; for suduko rows 0-8, 0-8, 0-8
-;	    constant    X_Table_length = 27
-;	    
-;;X_UMask_Table_PM  data 0xE0,0x83,0xFE, 0xFE,0xF8,0xE0, 0xE0,0x83,0xFE
-;X_UMask_Table_PM  data 0x07,0xC1,0x7F, 0x7F,0x1F,0x07, 0x07,0xC1,0x7F
-;	    ; data masks for upper part of 5x5 cells
-;	    ; for suduko rows 0-8
-;	    constant    X_UMask_Table_length = 9
-;	    
-;;X_LMask_Table_PM  data 0xFF,0xFF,0x0F, 0x0F,0x3F,0x00, 0xFF,0xFF,0x0F
-;X_LMask_Table_PM  data 0xFF,0xFF,0xF0, 0xF0,0xFC,0xFF, 0xFF,0xFF,0xF0
-;	    ; data masks for lower part of 5x5 cells
-;	    ; for suduko rows 0-8
-;	    constant    X_LMask_Table_length = 9
-;	    
-;;Shapes_PM   data 0,0,0,0,0, 0,0x40,0x7C,0,0, 0xB8,0xA8,0xA8,0xA8,0xE8, 0x88,0x88,0xA8,0xA8,0xF8, 0xE0,0x20,0x20,0x20,0xF8 ,0xE8,0xA8,0xA8,0xA8,0xB8, 0xF8,0xA8,0xA8,0xA8,0xB8, 0x80,0x80,0x98,0xA0,0xC0, 0xF8,0xA8,0xA8,0xA8,0xF8, 0xE8,0xA8,0xA8,0xA8,0xF8
-;Shapes_PM1   data    0x00,0x00,0x00,0x00,0x00, 0x00,0x12,0x1F,0x10,0x00, 0x1D,0x15,0x15,0x15,0x17, 0x11,0x11,0x15,0x15,0x1F, 0x07,0x04,0x04,0x04,0x1F, 0x17,0x15,0x15,0x15,0x1D
-;Shapes_PM2   data    0x1F,0x15,0x15,0x15,0x1D, 0x01,0x01,0x19,0x05,0x03, 0x1F,0x15,0x15,0x15,0x1F, 0x17,0x15,0x15,0x15,0x1F, 0x1F,0x1F,0x1F,0x1F,0x1F
-;   ; 5 bytes of storage for shape paterns for each digit 0-9 and one solid block
-;	    constant	Shape_Table1_length = 30
-;	    constant	Shape_Table2_length = 25
-;   
-;Tables	udata	0x400    ; reserve data anywhere in RAM (here at 0x400)
-;Y_Table		res Y_Table_length    ; reserve 9 bytes for Column Look Up Table
-;X_Table		res X_Table_length	; reserve 27 bytes for Row Look Up Table
-;X_UMask_Table	res X_UMask_Table_length ; Mask storage
-;X_LMask_Table	res X_LMask_Table_length ; Mask storage
-;
-;Tables2	udata	0x500    ; reserve data anywhere in RAM (here at 0x400)
-;Shapes_Table	res Shape_Table1_length + Shape_Table2_length
-;	
-;acs0	    udata_acs
-;number_name res 1   ; Variables to be accessed from external programs
-;sudoku_x    res 1   ; sudoku coordinate of cell
-;sudoku_y    res 1   ; sudoku coordinate of cell
-;w_input	    res 1
-; 
-;Reading_Counter	res 1	
-;tlp_x		res 1	; x-pixel coordinate value of top left of 5x5 cell
-;tlp_y		res 1	; y-pixel coordinate value of top left of 5x5 cell
-;mask		res 1
-;rotate_counter	res 1
-;	
-;read_data	res 5
-;shifted_shape	res 5
-;	
-;counter		res 1
-;	
+Storage	code    ; a section of programme memory for storing data
+	; ******* myTable, data in programme memory, and its length *****
+C_PM data 0x38,0x44,0x44,0x28
+	constant    C_length = 4
+O_PM data 0x38,0x44,0x44,0x38
+	constant    O_length = 4
+R_PM data 0x7C,0x14,0x34,0x48
+	constant    R_length = 4
+E_PM data 0x7C,0x54,0x54,0x44
+	constant    E_length = 4
+T_PM data 0x04,0x04,0x7C,0x04,0x04
+	constant    T_length = 5
+	
+E_Mark_PM data 0x17
+	constant    E_Mark_length = 1
+	
+W_PM data 0x7C,0x20,0x10,0x20,0x7C
+	constant    W_length = 5
+N_PM data 0x7C,0x08,0x10,0x7C
+	constant    N_length = 4
+G_PM data 0x38,0x44,0x54,0x30
+	constant    G_length = 4
+	
+Tables2	udata_acs    ; reserve data anywhere in RAM (here at 0x400)
+counter res 1
 	
 Tables2	udata	0x300    ; reserve data anywhere in RAM (here at 0x400)
-;correct_PM	res .38
-;wrong_PM	res .28
 row_message	res .64
 	
 Main	code
@@ -80,13 +40,217 @@ write_correct
 	movlw	0x00
 	call	Set_GLCD_Cursor_Y
 	
-	lfsr	
+	lfsr	2, row_message
+	
+	movlw	.13
+	call	w_padding
+	
+	movlw	upper(C_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(C_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(C_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	C_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(O_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(O_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(O_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	O_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(R_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(R_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(R_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	R_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(R_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(R_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(R_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	R_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(E_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(E_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(E_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	E_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(C_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(C_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(C_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	C_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(T_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(T_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(T_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	T_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(E_Mark_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(E_Mark_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(E_Mark_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	E_Mark_length	; bytes to read
+	call	load_letter
+	
+	movlw	.13
+	call	w_padding
+	
+	lfsr	2, row_message
 	call	Write_Block_2
 	
 	return
 	
 write_wrong
 	
+	movlw	0x03
+	call	Set_GLCD_Cursor_X
+	movlw	0x00
+	call	Set_GLCD_Cursor_Y
+	
+	lfsr	2, row_message
+	
+	movlw	.18
+	call	w_padding
+	
+	movlw	upper(W_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(W_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(W_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	W_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(R_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(R_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(R_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	R_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(O_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(O_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(O_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	O_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(N_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(N_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(N_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	N_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(G_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(G_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(G_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	G_length	; bytes to read
+	call	load_letter
+	
+	movlw	0x00
+	movwf	POSTINC2
+	
+	movlw	upper(E_Mark_PM)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(E_Mark_PM)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(E_Mark_PM)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	E_Mark_length	; bytes to read
+	call	load_letter
+	
+	
+	movlw	.18
+	call	w_padding
+	
+	lfsr	2, row_message
+	call	Write_Block_2
+	
+	return
+	
+w_padding   ; Pad FSP2 with W number of 0x00 bytes
+	movwf	counter
+wplp
+	movlw	0x00
+	movwf	POSTINC2
+	decfsz	counter
+	bra	wplp
+	return
+	
+load_letter
+	movwf 	counter	; our counter register
+lllp 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+	movff	TABLAT, POSTINC2; move data from TABLAT to (FSR2), inc FSR2
+	decfsz	counter	; count down to zero
+	bra	lllp		; keep going until finished
 	return
 
 	end
